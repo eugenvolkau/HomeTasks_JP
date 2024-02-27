6e45965e7271efa57321687d345613f2c225658c
Eugen <eugen.volkau@gmail.com>
2024-02-27 09:50:39 +0100

    Das ist 19.02  Home task ALPHABET second abgrade

--- src/main/java/com/telran/prof/hometask/alfabet/EnglishAlphsbet.java
+++ src/main/java/com/telran/prof/hometask/alfabet/EnglishAlphsbet.java
@@ -3,17 +3,17 @@ package com.telran.prof.hometask.alfabet;
 import java.util.Arrays;
 
 public class AlphaBet_Eng extends Alphabet {
-    char[] arrchar_eng = new char[26];
+    char[] arrcharEng = new char[26];
 
-    public char[] getArrchar_eng() {
-        return arrchar_eng;
+    public char[] getArrcharEng() {
+        return arrcharEng;
     }
 
     @Override
     public void printAlphabet() {
         for (int i = 0; i < 26; i++) {
-            arrchar_eng[i] = (char) (i + 97);
+            arrcharEng[i] = (char) (i + 97);
         }
-        System.out.println(Arrays.toString(arrchar_eng));
+        System.out.println(Arrays.toString(arrcharEng));
     }
 }
