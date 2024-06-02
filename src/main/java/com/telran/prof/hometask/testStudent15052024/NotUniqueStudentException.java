f8705781531dc77f6eb9bc3753c320b8c2996a98
Eugen <eugen.volkau@gmail.com>
2024-06-02 11:34:27 +0200

    There are some unit tests with a wrong, which i plane bring to consultation.

---
+++ src/main/java/com/telran/prof/hometask/testStudent15052024/NotUniqueStudentException.java
@@ -0,0 +1,9 @@
+package com.telran.prof.hometask.testStudent15052024;
+
+public class NotUniqueStudentException extends RuntimeException{
+
+    public NotUniqueStudentException(String message){
+        super(message);
+    }
+
+}
