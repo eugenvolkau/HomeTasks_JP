3a0e54fdd74f09cfc9f0b1d22ad47ce4e3a37087
Eugen <eugen.volkau@gmail.com>
2024-07-08 13:29:57 +0200

    I should also deal with AtomicBoolean. I still vaguely imagine an algorithm with AtomicBoolean

---
+++ src/main/java/com/telran/prof/hometask/EventProcessArray/WriterOfEvent.java
@@ -0,0 +1,58 @@
+package com.telran.prof.hometask.EventProcessArray;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class WriterOfEvent extends Thread {
+
+    private List<List<String>> list;
+
+    public WriterOfEvent(List<List<String>> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        sleepNow(1);
+        while (true) {
+
+            synchronized (list) {
+
+                if (list.size() > 0) {
+                    List<String> s = list.get(0);
+
+                    System.out.println("incomen list  " + list + " -> " + EventCreator.eventCounter);
+                    Iterator<String> itera = s.iterator();
+                    List<String> list1 = new ArrayList<>();
+                    while (itera.hasNext()) {
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("Handler ").append(Thread.currentThread().getName() + "   id=").append(EventCreator.eventCounter + " - ").append(itera.next());
+
+                        list1.add(sb.toString());
+                    }
+                    list1.stream().forEach(String -> System.out.println(String));
+
+                    list.remove(0);
+                    System.out.println("Size of list = " + list.size());
+                } else {
+                    System.out.println("Consumer  -->");
+                    System.out.println("Check list. List is Emty");
+                    try {
+                        list.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void sleepNow(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
